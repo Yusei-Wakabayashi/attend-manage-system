51890da9d79862e2495f06d7bf169e5bdbfe1164
Yokoji0129 <132248053+Yokoji0129@users.noreply.github.com>
2025-09-06 11:00:59 +0900

    ログイン時ID,passなしで一旦は入れるようにした。 APi名変更

--- frontend/src/components/Popup.vue
+++ frontend/src/components/Popup.vue
@@ -1,4 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+
+const shiftList = ref([
+  {
+    id: 1,
+    beginWork: '2025-09-04T09:00:00',
+    endWork: '2025-09-04T18:00:00',
+    beginBreak: '2025-09-04T12:00:00',
+    endBreak: '2025-09-04T13:00:00',
+    lateness: 5,
+    leaveEarly: 10,
+    outing: 30,
+    overWork: 60,
+  },
+])
 </script>
 
 <template>
@@ -6,38 +21,17 @@
     <div
       class="bg-white border-2 border-green-500 w-50 h-auto p-4"
     >
-       <div class="flex justify-between mb-2">
-        <span>始業時刻:</span>
-        <span>12:00</span>
-      </div>
-      <div class="flex justify-between mb-2">
-        <span>終業時刻:</span>
-        <span>17:00</span>
-      </div>
-      <div class="flex justify-between mb-2">
-        <span>休憩開始時刻:</span>
-        <span>15:00</span>
-      </div>
-      <div class="flex justify-between mb-2">
-        <span>休憩終了時刻:</span>
-        <span>15:30</span>
-      </div>
-      <div class="flex justify-between mb-2">
-        <span>遅刻予定時間:</span>
-        <span>12:30</span>
-      </div>
-      <div class="flex justify-between mb-2">
-        <span>早退予定時間:</span>
-        <span>16:30</span>
-      </div>
-      <div class="flex justify-between mb-2">
-        <span>外出予定時間:</span>
-        <span>00:00</span>
-      </div>
-      <div class="flex justify-between mb-2">
-        <span>残業予定時間:</span>
-        <span>00:00</span>
-      </div>
+          <div class="" v-for="(shift, index) in shiftList" :key="index">
+            <p><strong>id:</strong> {{ shift.id }}</p>
+            <p><strong>beginWork:</strong> {{ shift.beginWork }}</p>
+            <p><strong>endWork:</strong> {{ shift.endWork }}</p>
+            <p><strong>beginBreak:</strong> {{ shift.beginBreak }}</p>
+            <p><strong>endBreak:</strong> {{ shift.endBreak }}</p>
+            <p><strong>lateness:</strong> {{ shift.lateness }} 分</p>
+            <p><strong>leaveEarly:</strong> {{ shift.leaveEarly }} 分</p>
+            <p><strong>outing:</strong> {{ shift.outing }} 分</p>
+            <p><strong>overWork:</strong> {{ shift.overWork }} 分</p>
+          </div>
     </div>
   </div>
 </template>
