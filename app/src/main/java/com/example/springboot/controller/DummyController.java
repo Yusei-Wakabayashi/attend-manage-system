f06a05e8dcf156c47e84ec7e1fd5d1988d2a4a31
DESKTOP-18U274T\fowak <fowakuma3@icloud.com>
2025-11-29 14:00:05 +0900

    dummyapiに情報を追加しました

--- app/src/main/java/com/example/springboot/controller/DummyController.java
+++ app/src/main/java/com/example/springboot/controller/DummyController.java
@@ -158,11 +158,11 @@ public class DummyController
     @GetMapping("/reach/requestlist")
     public ArrayResponse<RequestListResponse> returnRequests()
     {
-        RequestListResponse approvalRequestListResponse = new RequestListResponse();
-        RequestListResponse rejectRequestListResponse = new RequestListResponse();
-        RequestListResponse approvalCancelRequestListResponse = new RequestListResponse();
-        RequestListResponse withdrowRequestListResponse = new RequestListResponse();
-        RequestListResponse monthlyRequestListResponse = new RequestListResponse();
+        RequestListResponse approvalRequestListResponse = new RequestListResponse(2,2,"2025/12/12T11:22:03",2);
+        RequestListResponse rejectRequestListResponse = new RequestListResponse(4,3,"2025/12/12T11:22:03",3);
+        RequestListResponse approvalCancelRequestListResponse = new RequestListResponse(6,4,"2025/12/12T11:22:03",4);
+        RequestListResponse withdrowRequestListResponse = new RequestListResponse(3,5,"2025/12/12T11:22:03",5);
+        RequestListResponse monthlyRequestListResponse = new RequestListResponse(5,6,"2025/12/12T11:22:03",6);
         List<RequestListResponse> requestResponses = List.of(approvalRequestListResponse, rejectRequestListResponse, approvalCancelRequestListResponse, withdrowRequestListResponse, monthlyRequestListResponse);
         return new ArrayResponse<RequestListResponse>(1, requestResponses, "requestList");
     }
